211bff7d52180d5179486a85020f5e30c8cec599
ikeogu <ikeogu322@gmail.com>
2024-03-22 16:50:43 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,19 @@ use bdk::blockchain::ElectrumBlockchain;
 use bdk::electrum_client::Client;
 
 
-async fn main() {
-    
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  env_logger::init();
+  println!("Starting server at: http://127.0.0.1:8080");
 
+
+   
+  HttpServer::new(|| {
+      App::new()
+          .configure(configure_routes)
+  })
+  .bind("127.0.0.1:8080")?
+  .run()
+  .await
+}
 
