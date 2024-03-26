d4db69f8d82a7b40123a0e6649f202625ec20b6a
Isaack Njama <isaacknjama@proton.me>
2024-03-26 16:51:33 +0300

    feat: add test to get receiving address

--- src/tests/wallet_struct_test.rs
+++ src/tests/wallet_struct_test.rs
@@ -1,6 +1,13 @@
 #[cfg(test)]
 mod tests {
     use crate::wallet_struct::WalletStruct;
+    use bdk::{database::MemoryDatabase, Wallet};
+    use bip39::Seed;
+    use bitcoin::{
+        secp256k1,
+        util::bip32::{ChildNumber, ExtendedPrivKey, ExtendedPubKey},
+        Network,
+    };
 
     #[test]
     fn test_generate_mnemonic() {
@@ -19,7 +26,8 @@ mod tests {
     #[test]
     fn test_import_wallet() {
         // Define a sample mnemonic phrase
-        let mnemonic_phrase = "screen always funny riot garment emerge canvas insane chuckle ice decade cigar";
+        let mnemonic_phrase =
+            "screen always funny riot garment emerge canvas insane chuckle ice decade cigar";
 
         // Call the import_wallet function with the sample mnemonic phrase
         let imported_wallet = WalletStruct::import_wallet(mnemonic_phrase).unwrap();
@@ -30,5 +38,38 @@ mod tests {
         assert!(imported_wallet.public_key.is_some());
         assert!(imported_wallet.private_key.is_none()); // Assuming the private key is not returned
         assert_eq!(imported_wallet.mnemonic.unwrap(), mnemonic_phrase);
+    }
+
+    #[test]
+    fn test_get_address() {
+        let mnemonic = WalletStruct::generate_mnemonic().unwrap();
+        let seed = Seed::new(&mnemonic, "");
+        // Derive the master extended private key
+        let master_extended_private_key =
+            ExtendedPrivKey::new_master(Network::Testnet, &seed.as_bytes()).unwrap();
+        let extended_private_key = master_extended_private_key
+            .derive_priv(&secp256k1::Secp256k1::new(), &[ChildNumber::from_hardened_idx(84 + 0).unwrap()])
+            .unwrap();
+
+
+        // Derive the extended public key from the extended private key
+        let extended_public_key =
+            ExtendedPubKey::from_private(&secp256k1::Secp256k1::new(), &extended_private_key);
+
+        let descriptor= format!("wpkh({})", extended_public_key.to_string());
+
+        let database = MemoryDatabase::default();
+
+        let wallet =
+            Wallet::new(&descriptor, None, bdk::bitcoin::Network::Testnet, database).unwrap();
+
+        let address = match WalletStruct::get_address(&wallet) {
+            Ok(address) => address,
+            Err(err) => {
+                panic!("Failed to get address: {:?}", err);
+            }
+        };
+
+        assert!(address.starts_with("tb1"));
     }
 }
