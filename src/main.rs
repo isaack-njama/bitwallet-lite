a3c1ee4018d0eb9e130a2ef66e14b15ab2e57fb7
Isaack Njama <isaacknjama@proton.me>
2024-03-28 10:09:09 +0300

    feat: solve CORS error

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod wallet_struct_test;
 mod routes;
 pub mod wallet_struct;
 
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use routes::configure_routes;
 
@@ -13,8 +14,16 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
     println!("Starting server at: http://127.0.0.1:8080");
 
-    HttpServer::new(|| App::new().configure(configure_routes))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+                .allow_any_origin()
+                .allowed_methods(vec!["GET", "POST"])
+                .allow_any_header()
+                .max_age(3600);
+
+        App::new().wrap(cors).configure(configure_routes)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
