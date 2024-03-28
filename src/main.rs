7d4a9bbd0041ca2ebe35e981604f0aba33c9f9ff
ikeogu <ikeogu322@gmail.com>
2024-03-28 09:39:28 +0100

    still in progress

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
       App::new()
           .configure(configure_routes)
   })
-  .bind("127.0.0.1:8080")?
+  .bind("0.0.0.1:8080")?
   .run()
   .await
 }
